bb690dbcf9f4943c4d82c66113d34d2ab2818913
lochmidth <alphanogun@gmail.com>
2023-09-13 13:59:55 +0300

    Initial Commit

---
+++ UberFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberFirestore
+//
+//  Created by Alphan Ogün on 13.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
