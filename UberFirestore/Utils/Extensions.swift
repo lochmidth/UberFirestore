14974bbd4377b1619835a9ea3396fb2486932812
lochmidth <alphanogun@gmail.com>
2023-09-17 23:11:54 +0300

    Complete Location Input User Interface

--- UberFirestore/Utils/Extensions.swift
+++ UberFirestore/Utils/Extensions.swift
@@ -154,6 +154,14 @@ extension UIView {
         widthAnchor.constraint(equalToConstant: width).isActive = true
     }
     
+    func addShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.55
+        layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        layer.masksToBounds = false
+
+    }
+    
     func setHeight(_ height: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: height).isActive = true
